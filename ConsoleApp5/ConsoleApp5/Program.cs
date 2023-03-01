4a93a5320792331a1132a9a9d90ce21ae5a68195
Cavidan Shushayev <csusayev19@gmail.com>
2023-03-01 21:03:52 +0400

    last commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 24, 25, 26, 27, 28, 29, 30 };
+            int cem = 0;
+            int say = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                cem = cem + arr[i];
+                say ++;
+            }
+            Console.WriteLine(cem / say);
+        }
+    }
+}
---
+++ tekededcemi/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 13, 14, 15, 16 };
+            int cem = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] %2 != 0)
+                {
+                    cem = cem + arr[i];
+                }
+            }
+            Console.WriteLine(cem);
+        }
+    }
+}
